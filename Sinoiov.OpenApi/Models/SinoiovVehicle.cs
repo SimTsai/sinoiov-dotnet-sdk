ed5d764885fa813665b5e64616aeb79c52de96ad
Sim Tsai <13759975+SimTsai@users.noreply.github.com>
2021-06-23 10:32:42 +0800

    keep poco

--- Sinoiov.OpenApi/Models/SinoiovVehicle.cs
+++ Sinoiov.OpenApi/Models/SinoiovVehicle.cs
@@ -6,7 +6,7 @@ namespace Sinoiov.OpenApi.Models
     /// 车牌信息
     /// </summary>
     [System.Diagnostics.DebuggerDisplay("{VehicleNO}_{VehicleColor}")]
-    public partial class SinoiovVehicle : System.IComparable<SinoiovVehicle>, System.IEquatable<SinoiovVehicle>
+    public partial class SinoiovVehicle
     {
         /// <summary>
         /// 车牌号
@@ -16,34 +16,5 @@ namespace Sinoiov.OpenApi.Models
         /// 车牌颜色
         /// </summary>
         public virtual SinoiovVehicleColor VehicleColor { get; init; }
-
-        /// <summary>
-        /// IComparable
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public int CompareTo(SinoiovVehicle other) => this.VehicleNO.CompareTo(other.VehicleNO) + this.VehicleColor.CompareTo(other.VehicleColor);
-
-        /// <summary>
-        /// IEquatable
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(SinoiovVehicle other) => this.VehicleNO == other.VehicleNO && this.VehicleColor == other.VehicleColor;
-
-        /// <summary>
-        /// ==
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        public static bool operator ==(SinoiovVehicle a, SinoiovVehicle b) => a.Equals(b);
-        /// <summary>
-        /// !=
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        public static bool operator !=(SinoiovVehicle a, SinoiovVehicle b) => !a.Equals(b);
     }
 }
